79a61cf3d8ad677022f628f61990551ef8033d8c
mesbaulalam <mesbaul.a.khan@gmail.com>
2021-08-01 05:10:17 +0600

    Added links to technologies in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,21 @@ const Footer: React.FC = () => {
     <div className="text-center text-gray-400 pt-24 bg-gray-800 pb-10">
       <p>BUILT WITH THE FOLLOWING TECHNOLOGIES</p>
       <div className="flex flex-wrap items-center justify-around mt-10 text-6xl">
-        <i className="devicon-react-original-wordmark"></i>
-        <i className="devicon-nextjs-original-wordmark"></i>
-        <i className="devicon-typescript-plain"></i>
-        <i className="devicon-tailwindcss-plain"></i>
-        <i className="devicon-firebase-plain-wordmark"></i>
+        <a href="https://reactjs.org/" target="_blank">
+          <i className="devicon-react-original-wordmark"></i>
+        </a>
+        <a href="https://nextjs.org/" target="_blank">
+          <i className="devicon-nextjs-original-wordmark"></i>
+        </a>
+        <a href="https://www.typescriptlang.org/" target="_blank">
+          <i className="devicon-typescript-plain"></i>
+        </a>
+        <a href="https://tailwindcss.com/" target="_blank">
+          <i className="devicon-tailwindcss-plain"></i>
+        </a>
+        <a href="https://firebase.google.com/" target="_blank">
+          <i className="devicon-firebase-plain-wordmark"></i>
+        </a>
       </div>
     </div>
   );
